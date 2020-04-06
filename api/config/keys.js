cbc22e1d3d4f5eb8aad1d2b213cbb35d2ba9b0ce
Lovacaky13 <lovacaky13@gmail.com>
2020-04-06 10:19:31 +0200

    debut lightbox2

--- api/config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mongoUrl: "mongodb+srv://trotGo:Binet170578@cluster0-dtp3q.mongodb.net/test?retryWrites=true&w=majority",
-
-    nodemailerKeys: {
-        user: "lovacaky13@gmail.com",
-        pass: "17051978"
-
-    }
-}
