0b451a72872dbeb9a51b70e5199322ccabda9a69
Lovacaky13 <lovacaky13@gmail.com>
2020-05-26 17:41:49 +0200

    formulaire fiche client

--- api/db/models/Customer.js
+++ api/db/models/Customer.js
@@ -20,11 +20,61 @@ const mongoose = require('mongoose'),
         city: {
             type: String,
         },
+        tutor: {
+            type: String,
+        },
+        phone: {
+            type: String,
+        },
         email: {
             type: String,
             unique: true //l'email ne doit pas etre identique aux autres utilisateurs
         },
-
+        medical: {
+            type: String,
+        },
+        diet: {
+            type: String,
+        },
+        up: {
+            type: String,
+        },
+        cooked: {
+            type: String,
+        },
+        aesthetic: {
+            type: String,
+        },
+        cardGame: {
+            type: String,
+        },
+        game: {
+            type: String,
+        },
+        walk: {
+            type: String,
+        },
+        gardening: {
+            type: String,
+        },
+        sing: {
+            type: String,
+        },
+        manual: {
+            type: String,
+        },
+        memory: {
+            type: String,
+        },
+        gym: {
+            type: String,
+        },
+        mediaLibrary: {
+            type: String,
+        },
+        other: {
+            type: String,
+        },
     })
 
 
